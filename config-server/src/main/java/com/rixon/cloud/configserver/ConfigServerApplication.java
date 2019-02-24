8a826286b53a5c28ddd78c7d4bf5702c28847f94
rixonmathew <rixonmathew@gmail.com>
2019-02-24 18:21:39 +0530

    added config server

---
+++ config-server/src/main/java/com/rixon/cloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.rixon.cloud.configserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class).run(args);
+    }
+}
