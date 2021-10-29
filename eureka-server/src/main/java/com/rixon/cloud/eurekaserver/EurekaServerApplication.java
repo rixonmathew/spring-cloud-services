67933acf9012962105ea7f7fd695bf1849fb5f5d
rixon <rixonmathew@gmail.com>
2021-10-29 20:50:40 +0530

    updated spring boot parent

--- eureka-server/src/main/java/com/rixon/cloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/rixon/cloud/eurekaserver/EurekaServerApplication.java
@@ -1,13 +1,27 @@
 package com.rixon.cloud.eurekaserver;
 
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(EurekaServerApplication.class).run(args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CustomRequestDumperFilter> requestDumperFilter() {
+        FilterRegistrationBean<CustomRequestDumperFilter> registration = new FilterRegistrationBean<>();
+        CustomRequestDumperFilter requestDumperFilter = new CustomRequestDumperFilter();
+        registration.setFilter(requestDumperFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
     }
 }
